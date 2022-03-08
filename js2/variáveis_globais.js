aeb8a6e63624a3dbf96982f092576b9129a97fcf
hugoresende27 <Hugoresende27@gmail.com>
2022-03-08 11:46:16 +0000

    t

--- js2/variáveis_globais.js
+++ js2/variáveis_globais.js
@@ -12,8 +12,31 @@ var grupoCartas = [
  "🙈",
  "🐖"];
 
+ 
+// var totalCartas=grupoCartas.concat(grupoCartas);
 
+// var car1 = '<img src="images/car1.jpg" style="max-width:110px; max-height:110px"';
+// var car22 = '<img src="images/car22.jpg" style="max-width:110px; max-height:110px"';
+// var car3 = '<img src="images/car3.jpg" style="max-width:110px; max-height:110px"';
+// var car4 = '<img src="images/car4.jpg" style="max-width:110px; max-height:110px"';
+// var car5 = '<img src="images/car5.jpg" style="max-width:110px; max-height:110px"';
+// var car6 = '<img src="images/car6.jpg" style="max-width:110px; max-height:110px"';
+
+// var grupoCartas = [
+// car1,
+// car22,
+// car3,
+// car4,
+// car5,
+// car6,
+//  "🐖",
+//  "🐖",];
+
+ 
 var totalCartas=grupoCartas.concat(grupoCartas);
+
+
+
 var total = Number(0);
 
 document.getElementById('total').innerHTML = total;
@@ -48,4 +71,9 @@ function PlaySound(melody) {
     var path = "video"
     var snd = new Audio(path + melody + ".mp4");
     snd.play();
+}
+
+function duplica(){
+    // var totalCartas=grupoCartas.concat(grupoCartas,grupoCartas);
+    window.location.reload()
 }
