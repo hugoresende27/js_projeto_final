0eee54d752426024077b31fac5d3f586b9d4775c
hugoresende27 <Hugoresende27@gmail.com>
2022-03-08 12:24:13 +0000

    t

--- js2/cronometro.js
+++ js2/cronometro.js
@@ -1,10 +1,34 @@
-let segundos = 59;
-let minutos = 1;
+
+let segundos ;
+let minutos ;
+
+if (nivel == 0){
+
+     segundos = 59;
+     minutos = 0;
+        
+} 
+if (nivel == 1){
+
+     segundos = 29;
+     minutos = 0;
+        
+} 
+if (nivel == 2){
+
+     segundos = 15;
+     minutos = 0;
+        
+} 
+
+
 
 function segundo(){
     //incrementa os segundos
+    document.getElementById('minuto').innerHTML=minutos
     segundos--;
     if(segundos==00){
+    
         //incrementa os minutos
         minutos--;
         //Zerar os segundos
@@ -14,6 +38,11 @@ function segundo(){
     }
     //escreve os segundos
     document.getElementById('segundo').innerHTML=segundos
+    if ( minutos <0){
+        
+       
+        document.getElementById('msg2').style.visibility="visible"
+    }
     
 }
 
